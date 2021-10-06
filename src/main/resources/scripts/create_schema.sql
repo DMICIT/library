330fa08fad137acfef3b3485b2499010e7ca2343
Dmytro Mikhaltsov <mikhaltsovda@gmail.com>
2021-10-06 20:33:52 +0300

    add database scripts

---
+++ src/main/resources/scripts/create_schema.sql
@@ -0,0 +1,51 @@
+CREATE TABLE `users` (
+                         `id` int(11) NOT NULL AUTO_INCREMENT,
+                         `name` varchar(30) DEFAULT NULL,
+                         `email` varchar(40) DEFAULT NULL,
+                         `sex` enum ('man','woman') DEFAULT NULL,
+                         `telephone` int(15) DEFAULT NULL,
+                         `role` enum ('admin','user','librarian') default null,
+                         `bun_list` tinyint (1),
+                         PRIMARY KEY (`id`));
+
+CREATE TABLE `orders`(
+                         `id` int (11) NOT NULL auto_increment,
+                         `id_book` int (11) DEFAULT NULL,
+                         `id_user` int (11) DEFAULT NULL,
+                         `book_spot` enum ('abonement', 'library hall') DEFAULT NULL,
+                         `status` enum ('expected','checked out' ,'returned'),
+                         `date_of_service` int(11) DEFAULT NULL,
+                         PRIMARY KEY (`id`));
+
+CREATE TABLE `books`(
+                        `id` int (11) NOT NULL auto_increment,
+                        `author` varchar(30) DEFAULT NULL,
+                        `book_name` varchar(30) DEFAULT NULL,
+                        `book_edition` varchar (30) DEFAULT NULL,
+                        `realise_date` date DEFAULT NULL,
+                        PRIMARY KEY (`id`));
+
+CREATE TABLE `penalties`(
+                            `id` int (11) NOT NULL auto_increment,
+                            `user_id` int (11) DEFAULT NULL ,
+                            `order_id` int (11) DEFAULT NULL ,
+                            `penalty_cost` int (11) DEFAULT NULL,
+                            PRIMARY KEY (id),
+                            FOREIGN KEY (user_id) REFERENCES users (id),
+                            FOREIGN KEY (order_id) REFERENCES orders (id));
+
+
+CREATE TABLE `catalog`(
+                          `id` int (11) NOT NULL auto_increment,
+                          `book_id` int (11) DEFAULT NULL,
+                          `status` enum ('checked out', 'available'),
+                          PRIMARY KEY (id),
+                          FOREIGN KEY (book_id) REFERENCES books (id));
+
+
+
+
+
+
+
+
