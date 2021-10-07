13376d9a33b180fd2aec4f6f146692cb01bd3a7d
Dmytro Mikhaltsov <mikhaltsovda@gmail.com>
2021-10-07 12:52:54 +0300

    add entities

--- src/main/java/com/progect/entities/Book.java
+++ src/main/java/com/progect/entities/Book.java
@@ -1,2 +1,84 @@
-package com.progect.entities;public class Book {
+package com.progect.entities;
+
+import java.sql.Date;
+import java.util.Objects;
+
+public class Book {
+    private int id;
+    private String author;
+    private String bookName;
+    private String bookEdition;
+    private Date reliaseDate;
+
+    public Book(int id, String author, String bookName, String bookEdition, Date reliaseDate) {
+        this.id = id;
+        this.author = author;
+        this.bookName = bookName;
+        this.bookEdition = bookEdition;
+        this.reliaseDate = reliaseDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getBookEdition() {
+        return bookEdition;
+    }
+
+    public void setBookEdition(String bookEdition) {
+        this.bookEdition = bookEdition;
+    }
+
+    public Date getReliaseDate() {
+        return reliaseDate;
+    }
+
+    public void setReliaseDate(Date reliaseDate) {
+        this.reliaseDate = reliaseDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return id == book.id && Objects.equals(author, book.author) && Objects.equals(bookName, book.bookName) && Objects.equals(bookEdition, book.bookEdition) && Objects.equals(reliaseDate, book.reliaseDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, author, bookName, bookEdition, reliaseDate);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", author='" + author + '\'' +
+                ", bookName='" + bookName + '\'' +
+                ", bookEdition='" + bookEdition + '\'' +
+                ", reliaseDate=" + reliaseDate +
+                '}';
+    }
 }
--- src/main/java/com/progect/entities/User.java
+++ src/main/java/com/progect/entities/User.java
@@ -1,2 +1,105 @@
-package com.progect.entities;public class user {
+package com.progect.entities;
+
+import java.util.Objects;
+
+public class User {
+    private int id;
+    private String name;
+    private String email;
+    private String sex;
+    private String phone;
+    private String role;
+    private int banList;
+
+    public User(int id, String name, String email, String sex, String phone, String role, int banList) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.sex = sex;
+        this.phone = phone;
+        this.role = role;
+        this.banList = banList;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public int getBanList() {
+        return banList;
+    }
+
+    public void setBanList(int banList) {
+        this.banList = banList;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && banList == user.banList && Objects.equals(name, user.name) && Objects.equals(email, user.email) && Objects.equals(sex, user.sex) && Objects.equals(phone, user.phone) && Objects.equals(role, user.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, sex, phone, role, banList);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", sex='" + sex + '\'' +
+                ", phone='" + phone + '\'' +
+                ", role='" + role + '\'' +
+                ", banList=" + banList +
+                '}';
+    }
 }
